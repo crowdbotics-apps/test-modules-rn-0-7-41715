abb49928abba7ea259f1a09cc7925cce7654ad48
Saad bin Abid <saad.abid@crowdbotics.com>
2023-07-18 00:35:50 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "test-modules-rn-0-7-41715",
-  url: "https://test-modules-rn-0-7-41715.botics.co",
-  api: "https://test-modules-rn-0-7-41715.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "test-modules-rn-0-7-41715",
+  "url": "https://test-modules-rn-0-7-41715.botics.co",
+  "api": "https://test-modules-rn-0-7-41715.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
